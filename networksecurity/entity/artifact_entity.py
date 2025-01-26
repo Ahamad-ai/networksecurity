e44a5789aa38f2eb00bdcedacc69c26035fb7737
Ahamad-ai <ahamad.aihyd@gmail.com>
2025-01-26 11:39:05 +0530

    Data Transformation Commit

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -15,4 +15,11 @@ class DataValidationArtifact:
     invalid_test_file_path: str
     drift_report_file_papth: str
 
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
+
 
