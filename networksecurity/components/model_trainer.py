4db1bd3682576f4e81f3f1935afc0470d8da2ef7
Ahamad-ai <ahamad.aihyd@gmail.com>
2025-01-26 21:11:00 +0530

    pipeline is completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,6 +25,10 @@ from sklearn.ensemble import(
     RandomForestClassifier
 )
 import mlflow
+import dagshub
+dagshub.init(repo_owner='Ahamad-ai', repo_name='networksecurity', mlflow=True)
+
+
 
 class ModelTrainer:
     def __init__(self, model_trainer_config:ModelTrainerConfig,data_transformation_artifacts:DataTransformationArtifact):
@@ -118,6 +122,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+
+        save_object("final_model/model.pkl",best_model)
 
         #Model Trainer Artifact
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
